8b6a2b2a81b5277cc1908526b5d8559f9d752cc5
Carlos Almeida <carlos@carlosmcalmeida.pt>
2024-03-29 17:49:32 +0000

    feature: updated docker compose file

--- site/dockerbuild.sh
+++ site/dockerbuild.sh
@@ -2,4 +2,4 @@ sudo docker stop portfolio-site
 sudo docker rm portfolio-site
 sudo docker image rm portfolio-carlos-almeida:v1
 docker build -t portfolio-carlos-almeida:v1 .
-sudo docker run -d --name portfolio-site --network ubuntu_gitea -p 8090:80 portfolio-carlos-almeida:v1
+sudo docker run -d --name portfolio-site --network infrastructure -p 8080:80 portfolio-carlos-almeida:v1
