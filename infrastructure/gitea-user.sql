d6f1913a954046445ab96bef56ad7d7901554fc4
Carlos Almeida <carlos@carlosmcalmeida.pt>
2024-03-29 17:43:08 +0000

    feature: added SQL script to create Gitea's database user

---
+++ infrastructure/gitea-user.sql
@@ -0,0 +1,4 @@
+-- Replace 'hostname' with the gitea server's Docker network IP 
+
+CREATE USER 'gitea_user'@'hostname' IDENTIFIED BY 'password';
+GRANT INDEX, CREATE, SELECT, DELETE, DROP, UPDATE, INSERT  ON gitea.* TO 'gitea_user'@'hostname';
