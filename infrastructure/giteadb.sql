51f2056552faf131fbeb34990f5a05758aa93c14
Carlos Almeida <carlos@carlosmcalmeida.pt>
2024-03-29 18:10:29 +0000

    feature: updated database script

--- infrastructure/giteadb.sql
+++ infrastructure/giteadb.sql
@@ -1,4 +1,5 @@
 -- Replace 'hostname' with the gitea server's Docker network IP 
 
+CREATE DATABASE gitea;
 CREATE USER 'gitea_user'@'hostname' IDENTIFIED BY 'password';
 GRANT INDEX, CREATE, SELECT, DELETE, DROP, UPDATE, INSERT  ON gitea.* TO 'gitea_user'@'hostname';
